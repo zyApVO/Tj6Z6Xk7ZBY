699fd22feb05ae35147f4258849a74ede8096baf
zyApVO <112759538+zyApVO@users.noreply.github.com>
2025-01-01 02:47:05 +0100

    增加了 test_login 单元测试

---
+++ src/Code/admin.java
@@ -0,0 +1,16 @@
+
+package Code;
+
+
+public class admin {
+    private String AdminUsername;
+    private String AdminPassword;
+    
+    public void login() {
+        
+    }
+    
+ 
+    
+    
+}
