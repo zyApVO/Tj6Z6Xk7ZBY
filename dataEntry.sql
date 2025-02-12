6f22d2be53333ba253db8a8ceedaa82bca9d0f8d
zyApVO <112759538+zyApVO@users.noreply.github.com>
2025-02-12 05:28:03 +0100

    合并了 feature-branch 分支

---
+++ dataEntry.sql
@@ -0,0 +1,27 @@
+INSERT INTO trend (title, link, picture) VALUES ('Russian Debian-derivative Linux slinger plans IPO', 'https://www.theregister.com/2022/07/09/russian_debianderivative_vendor_plans_ipo/', 'https://thumbs.dreamstime.com/b/belgrade-serbia-january-vladimir-putin-president-russian-federation-press-conference-palace-ser-working-visit-137032566.jpg');
+
+INSERT INTO trend (title, link, picture) VALUES ('Markov Chat Bot Disaster Story', 'https://gist.github.com/aconbere/1982a5eb17b77817017a3da50914732f', 'https://cdn.neow.in/news/images/uploaded/2021/04/1619644762_github-desktop_story.jpg');
+
+INSERT INTO trend (title, link, picture) VALUES ('Custome JWT Token And ASP.NET Core Web API', 'https://www.c-sharpcorner.com/article/custome-jwt-token-and-asp-net-core-web-api/', 'https://ies.solutions/wordpress/wp-content/uploads/jwt.png');
+
+INSERT INTO trend (title, link, picture) VALUES ('Tech Transforms podcast: DevOps, Remote Work', 'https://www.dynatrace.com/news/blog/tech-transforms-may-2022/', 'https://marvel-b1-cdn.bc0a.com/f00000000236551/dt-cdn.net/wp-content/uploads/2022/06/Picture5.png');
+
+INSERT INTO trend (title, link, picture) VALUES ('Why we are sticking with Ruby on Rails', 'https://about.gitlab.com/blog/2022/07/06/why-were-sticking-with-ruby-on-rails/', 'https://about.gitlab.com/images/blogimages/gitlab-ruby.jpg');
+
+INSERT INTO trend (title, link, picture) VALUES ('MEAN vs MERN Stack: Which One Is Better?', 'https://dzone.com/articles/mean-vs-mern-stack-which-one-is-better', 'https://dz2cdn1.dzone.com/storage/temp/15973127-1655361511036.png');
+
+INSERT INTO trend (title, link, picture) VALUES ('Awesome PHP Weekly', 'https://php.libhunt.com/newsletter/320', 'https://www.php.net/images/meta-image.png');
+
+INSERT INTO trend (title, link, picture) VALUES ('A new approach to React Native development and team composition', 'https://itnext.io/a-new-approach-to-react-native-development-and-team-composition-d6e3e3eb5184', 'https://cdn.evilmartians.com/front/posts/optimizing-react-virtual-dom-explained/cover-a1d5b40.png');
+
+INSERT INTO trend (title, link, picture) VALUES ('Amazon Aurora Serverless v1 supports in-place upgrade from MySQL 5.6 to 5.7', 'https://aws.amazon.com/about-aws/whats-new/2022/06/amazon-aurora-serverless-v1-in-place-upgrade-mysql-5-6-5-7/', 'https://www.theeggbrussels.com/wp-content/uploads/2018/05/logo-AWS.png');
+
+INSERT INTO trend (title, link, picture) VALUES('Significant Improvements Made to Ubuntu Firefox Snap', 'https://www.omgubuntu.co.uk/2022/07/firefox-snap-significant-startup-improvements', 'https://imageio.forbes.com/blogs-images/jasonevangelho/files/2018/07/ubuntu-logo.jpg?format=jpg&width=1200');
+
+INSERT INTO trend (title, link, picture) VALUES ('Elastic Stack 8.3.2 released', 'https://www.elastic.co/blog/elastic-stack-8-3-2-released', 'https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt6a26a8ec94e87272/6001ac6a96de49101cfb1e46/cta-illustration-space-launch-white-bg.png');
+
+
+INSERT INTO trend (title, link, picture) VALUES ('FOSS4Spectroscopy: R vs Python', 'https://www.r-bloggers.com/2022/07/foss4spectroscopy-r-vs-python/', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMa3dV8eilVNUwdakPAwhhg0-PFC67HBhmQw&usqp=CAU');
+
+
+
