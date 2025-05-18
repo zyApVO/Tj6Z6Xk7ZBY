aaf57dbb50f6976b843b2b1b62ed8b01e1e38f2e
zyApVO <112759538+zyApVO@users.noreply.github.com>
2025-05-18 17:24:39 +0200

    改进了 validate_user 函数的实现

---
+++ AIRST.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# Create a Streamlit app with sidebar
+
+def main():
+    st.title(':medical_symbol: :blue[AI R]esearch :blue[S]ummary :blue[T]ool   	')
+    st.write('Welcome to our AI Research Summary Tool! Our app uses state-of-the-art AI technology to help healthcare professionals quickly and easily access and understand the latest research and findings related to healthcare.')
+    st.write('Our tool is designed to convert research articles into a variety of formats that are easy to understand and consume. This tool will allow healthcare professionals to quickly and easily access and digest the latest research and findings, as well as create informative and engaging presentations for their colleagues and patients.')
+    
+if __name__ == '__main__':
+    main()
